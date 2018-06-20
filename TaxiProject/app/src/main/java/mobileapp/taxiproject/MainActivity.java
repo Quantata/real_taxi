97272aef99821052168fcb1494d99d7acdd30454
DESKTOP-FFLFGB0\ceoah <ceoahn95@gmail.com>
2018-06-21 01:20:59 +0900

    나연_사진회전(갤러리, 카메라)

--- TaxiProject/app/src/main/java/mobileapp/taxiproject/MainActivity.java
+++ TaxiProject/app/src/main/java/mobileapp/taxiproject/MainActivity.java
@@ -2,7 +2,11 @@ package mobileapp.taxiproject;
 
 import android.Manifest;
 import android.content.Intent;
+import android.database.Cursor;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
+import android.media.ExifInterface;
 import android.net.Uri;
 import android.os.Environment;
 import android.provider.MediaStore;
@@ -86,6 +90,7 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    Uri imageUri;
     private File createImageFile() throws IOException {
         //Create an image file name
         String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
@@ -96,8 +101,20 @@ public class MainActivity extends AppCompatActivity {
         if (!directory_TaxiProject.exists())
             directory_TaxiProject.mkdir();
 
+        imageUri = Uri.fromFile(storageDir);
+
         return storageDir;
     }
+
+    public String getPath(Uri uri) {
+        String[] projection = {MediaStore.Images.Media.DATA};
+        Cursor cursor = managedQuery(uri, projection, null, null, null);
+        startManagingCursor(cursor);
+        int columnIndex = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+        cursor.moveToFirst();
+        return cursor.getString(columnIndex);
+    }
+
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
@@ -107,8 +124,20 @@ public class MainActivity extends AppCompatActivity {
         switch (requestCode) {
             case CAMERA_REQUEST_CODE:
                 try {
-                    Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), uri);
+                    String imagePath = imageUri.getPath();
+
+                    Bitmap bitmap = BitmapFactory.decodeFile(imagePath);
+
+                    bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), uri);
+                    ExifInterface exif = new ExifInterface(String.valueOf(imagePath));
+                    int exifOrientation = exif.getAttributeInt(ExifInterface.TAG_ORIENTATION, ExifInterface.ORIENTATION_NORMAL);
+                    int exifDegree = exifOrientationToDegrees(exifOrientation);
+                    bitmap = rotate(bitmap, exifDegree);
                     imageIv.setImageBitmap(bitmap);
+
+
+
+
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
@@ -117,14 +146,74 @@ public class MainActivity extends AppCompatActivity {
             case GALLERY_REQUEST_CODE:
                 uri = data.getData();
                 try {
+//                    Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), uri);
+//                    imageIv.setImageBitmap(bitmap);
+
+                    String path = getPath(uri);
+
                     Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), uri);
+                    ExifInterface exif = new ExifInterface(path);
+                    int exifOrientation = exif.getAttributeInt(ExifInterface.TAG_ORIENTATION, ExifInterface.ORIENTATION_NORMAL);
+                    int exifDegree = exifOrientationToDegrees(exifOrientation);
+                    bitmap = rotate(bitmap, exifDegree);
                     imageIv.setImageBitmap(bitmap);
+
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
                 break;
         }
     }
+    public int exifOrientationToDegrees(int exifOrientation)
+    {
+        if(exifOrientation == ExifInterface.ORIENTATION_ROTATE_90)
+        {
+            return 90;
+        }
+        else if(exifOrientation == ExifInterface.ORIENTATION_ROTATE_180)
+        {
+            return 180;
+        }
+        else if(exifOrientation == ExifInterface.ORIENTATION_ROTATE_270)
+        {
+            return 270;
+        }
+        return 0;
+    }
+
+    /**
+     * 이미지를 회전시킵니다.
+     *
+     * @param bitmap 비트맵 이미지
+     * @param degrees 회전 각도
+     * @return 회전된 이미지
+     */
+    public Bitmap rotate(Bitmap bitmap, int degrees)
+    {
+        if(degrees != 0 && bitmap != null)
+        {
+            Matrix m = new Matrix();
+            m.setRotate(degrees, (float) bitmap.getWidth() / 2,
+                    (float) bitmap.getHeight() / 2);
+
+            try
+            {
+                Bitmap converted = Bitmap.createBitmap(bitmap, 0, 0,
+                        bitmap.getWidth(), bitmap.getHeight(), m, true);
+                if(bitmap != converted)
+                {
+                    bitmap.recycle();
+                    bitmap = converted;
+                }
+            }
+            catch(OutOfMemoryError ex)
+            {
+                // 메모리가 부족하여 회전을 시키지 못할 경우 그냥 원본을 반환합니다.
+            }
+        }
+        return bitmap;
+    }
+
 
     public void init() {
         PermissionListener permissionListener = new PermissionListener() {
