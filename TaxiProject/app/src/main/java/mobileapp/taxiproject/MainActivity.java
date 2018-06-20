7e5f9e1fe3ee1c54589c8fb268a98717a9b634b2
DESKTOP-FFLFGB0\ceoah <ceoahn95@gmail.com>
2018-06-20 22:08:07 +0900

    nayeon_resultView_신고하기 완성

--- TaxiProject/app/src/main/java/mobileapp/taxiproject/MainActivity.java
+++ TaxiProject/app/src/main/java/mobileapp/taxiproject/MainActivity.java
@@ -1,16 +1,50 @@
 package mobileapp.taxiproject;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import javax.xml.transform.Result;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button detect;
+    Button gallery;
+    Button camera;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+       detect = (Button) findViewById(R.id.detect);
+       gallery = (Button) findViewById(R.id.gallery);
+       camera = (Button) findViewById(R.id.camera);
+
+
+//        detect.setOnClickListener((View.OnClickListener) this);
+//        gallery.setOnClickListener((View.OnClickListener) this);
+//        camera.setOnClickListener((View.OnClickListener)this);
+
+
         startActivity(new Intent(this, SplashActivity.class));
+
+        detect.setOnClickListener(new Button.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Intent intent = new Intent(getApplicationContext(), ResultActivity.class);
+                startActivity(intent);
+
+            }
+        });
+
+
+
+
     }
+
+
 }
--- TaxiProject/app/src/main/java/mobileapp/taxiproject/ResultActivity.java
+++ TaxiProject/app/src/main/java/mobileapp/taxiproject/ResultActivity.java
@@ -1,13 +1,55 @@
 package mobileapp.taxiproject;
 
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class ResultActivity extends AppCompatActivity {
+public class ResultActivity extends AppCompatActivity implements View.OnClickListener {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
+
+        Button emergency = (Button) findViewById(R.id.emergency);
+        Button backhome = (Button) findViewById(R.id.backhome);
+        TextView textResult = (TextView) findViewById(R.id.resultText);
+
+        emergency.setOnClickListener(this);
+        backhome.setOnClickListener(this);
+
+        boolean isTaxi = false;
+
+
+        if(isTaxi==true){
+            emergency.setVisibility(View.INVISIBLE);
+            textResult.setText("택시가 맞습니다!");
+        }
+        else{
+            emergency.setVisibility(View.VISIBLE);
+            textResult.setText("헉!택시가 아닙니다! 조심하세요!");
+        }
+    }
+
+    public void onClick(View v){
+        switch (v.getId()){
+
+            case R.id.emergency:
+                startActivity(new Intent("android.intent.action.DIAL",
+                        Uri.parse("tel:112")));
+                break;
+
+            case R.id.backhome:
+//                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+//                startActivity(intent);
+                onBackPressed();
+                break;
+        }
     }
 }
